4277f6e67b7eba5cbbe0377d357d527bc9357d03
maflAT <13979550+maflAT@users.noreply.github.com>
2022-12-10 10:09:09 +0100

    day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,5 +1,19 @@
 input = open("input.txt").readlines()
 
+values = [1]
+for cmd in input:
+    values.append(values[-1])
+    if cmd.startswith("addx"):
+        values.append(values[-1] + int(cmd.split()[1]))
+res1 = sum(values[i - 1] * i for i in [20, 60, 100, 140, 180, 220])
+
+res2 = "\n".join(
+    "".join(
+        "#" if abs(value - i) <= 1 else " "
+        for i, value in enumerate(values[i : min(i + 40, len(values))])
+    )
+    for i in range(0, len(values), 40)
+)
 
 print(f"Result 1: {res1}")
-print(f"Result 2: {res2}")
+print(f"Result 2:\n{res2}")
