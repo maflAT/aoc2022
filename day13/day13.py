c18397cb793d0217b10eddda517c4132f7bd7dd4
Markus Fleischhacker <markus.fleischhacker@magna.com>
2022-12-13 11:48:35 +0100

    day 13

--- day13/day13.py
+++ day13/day13.py
@@ -1,3 +1,4 @@
+from ast import literal_eval
 from itertools import zip_longest
 
 
@@ -19,9 +20,27 @@ def is_ordered(left, right) -> bool | None:
 
 
 input = open("input.txt").read()
-input_pairs = [[eval(l.strip()) for l in p.split()] for p in input.split("\n\n")]
+input_pairs = [
+    [literal_eval(l.strip()) for l in p.split()] for p in input.split("\n\n")
+]
 
 ordered = [is_ordered(pair[0], pair[1]) for pair in input_pairs]
 res1 = sum(i for i, p in enumerate(ordered, 1) if p)
 print(f"Result 1: {res1}")
-# print(f"Result 2:\n{res2}")
+
+# -------------------------------------- Part 2: ------------------------------------- #
+class Packet:
+    def __init__(self, packet: list | int) -> None:
+        self.p = packet
+
+    def __lt__(self, other: "Packet") -> bool:
+        return is_ordered(self.p, other.p)
+
+
+div1, div2 = Packet([[2]]), Packet([[6]])
+packets = sorted(
+    [Packet(literal_eval(l.strip())) for l in input.split() if l != "\n\n"]
+    + [div1, div2]
+)
+res2 = (packets.index(div1) + 1) * (packets.index(div2) + 1)
+print(f"Result 2:{res2}")
