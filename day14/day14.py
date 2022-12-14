4bb422441bd51bc29c972a6921e33e0946f2c14f
maflAT <13979550+maflAT@users.noreply.github.com>
2022-12-14 13:15:39 +0100

    day 14 changed tuples to complex

--- day14/day14.py
+++ day14/day14.py
@@ -1,22 +1,24 @@
-from itertools import pairwise, product
+from itertools import pairwise
 
 input = open("input.txt").readlines()
-filled: set[tuple[int, int]] = set()
-for e in [[tuple(map(int, c.split(","))) for c in l.split("->")] for l in input]:
-    for (x1, y1), (x2, y2) in pairwise(e):
+filled: set[complex] = set()
+for edge in [[tuple(map(int, c.split(","))) for c in l.split("->")] for l in input]:
+    for (x1, y1), (x2, y2) in pairwise(edge):
         x1, x2 = sorted((x1, x2))
         y1, y2 = sorted((y1, y2))
-        filled.update(product(range(x1, x2 + 1), range(y1, y2 + 1)))
+        filled.update(
+            complex(r, i) for r in range(x1, x2 + 1) for i in range(y1, y2 + 1)
+        )
 
-bottom = max(y for _, y in filled)
+bottom = max(p.imag for p in filled) + 1
 count1 = count2 = 0
-old = start = (500, 0)
+old = start = 500 + 0j
+offsets = (0 + 1j, -1 + 1j, 1 + 1j)
 while start not in filled:
-    x, y = old[0], old[1] + 1
-    new = next(((x + dx, y) for dx in [0, -1, 1] if (x + dx, y) not in filled), old)
-    if count1 == 0 and new[1] == bottom:
+    new = next((old + o for o in offsets if old + o not in filled), old)
+    if count1 == 0 and new.imag == bottom - 1:
         count1 = count2
-    if new == old or new[1] > bottom:
+    if new == old or new.imag > bottom:
         filled.add(old)
         count2 += 1
         old = start
@@ -25,7 +27,3 @@ while start not in filled:
 
 print(f"Result 1: {count1}")
 print(f"Result 2: {count2}")
-# for y in range(bottom + 1):
-#     for x in range(490, 510):
-#         print("#" if (x, y) in filled else ".", end="")
-#     print()
