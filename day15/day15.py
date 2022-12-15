845cfe1bbdf16c7bee49748685dfe13d31c2b304
maflAT <13979550+maflAT@users.noreply.github.com>
2022-12-15 22:01:16 +0100

    day 15 optimizations

--- day15/day15.py
+++ day15/day15.py
@@ -1,10 +1,12 @@
 import re
+import time
+from itertools import chain, product
 from typing import Iterable
+
+t = time.perf_counter()
 
 
 class Reading:
-    __slots__ = ["sx", "sy", "bx", "by", "rx", "ry", "range"]
-
     def __init__(self, sx: str, sy: str, bx: str, by: str) -> None:
         self.sx = int(sx)
         self.sy = int(sy)
@@ -14,47 +16,56 @@ class Reading:
         self.ry = abs(self.by - self.sy)
         self.range = self.rx + self.ry
 
-    def row_coverage(self, row: int) -> tuple[int, int] | None:
-        if abs(row - self.sy) > self.range:
-            return None
-        hor_range = self.range - abs(row - self.sy)
-        return self.sx - hor_range, self.sx + hor_range
+    def __lt__(self, other: "Reading") -> bool:
+        return self.range < other.range
 
+    def covers(self, x: int, y: int) -> bool:
+        return abs(x - self.sx) + abs(y - self.sy) <= self.range
 
-def merge_ranges(ranges: Iterable[tuple[int, int]]) -> list[tuple[int, int]]:
-    sr_iter = iter(sorted(ranges))
-    cur_low, cur_high = next(sr_iter)
-    res: list[tuple[int, int]] = []
-    for low, high in sr_iter:
-        if low <= cur_high + 1:
-            cur_high = max(cur_high, high)
-        else:
-            res.append((cur_low, cur_high))
-            cur_low, cur_high = low, high
-    res.append((cur_low, cur_high))
-    return res
+    def rim(self) -> Iterable[tuple[int, int]]:
+        return chain(
+            *[
+                zip(
+                    range(self.sx, self.sx + a * (self.range + 2), a),
+                    reversed(range(self.sy, self.sy + b * (self.range + 2), b)),
+                )
+                for a, b in product((1, -1), repeat=2)
+            ]
+        )
 
 
 input = open("input.txt").read()
 pattern = r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"
-readings = list(Reading(*m) for m in re.findall(pattern, input))
+readings = sorted((Reading(*m) for m in re.findall(pattern, input)), reverse=True)
 
 # -------------------------------------- Part 1: ------------------------------------- #
 row = 2_000_000
 beacons = len({(r.bx, r.by) for r in readings if r.by == row})
-rc = merge_ranges(r.row_coverage(row) for r in readings if r.row_coverage(row))
-res1 = sum(h - l + 1 for l, h in rc) - beacons
-print(f"Result 1: {res1}")
+ranges = sorted(
+    (r.sx - (hr := r.range - abs(row - r.sy)), r.sx + hr)
+    for r in readings
+    if abs(row - r.sy) <= r.range
+)
+
+cur_low, cur_high = ranges[0]
+coverage = 0
+for low, high in ranges[1:]:
+    if low <= cur_high + 1:
+        cur_high = max(cur_high, high)
+    else:
+        coverage += cur_high - cur_low + 1
+        cur_low, cur_high = low, high
+coverage += cur_high - cur_low + 1
+res1 = coverage - beacons
+print(f"Result 1: {res1} in {time.perf_counter() - t:.3f} s")
 
 # -------------------------------------- Part 2: ------------------------------------- #
-for row in range(4_000_000):
-    rc = merge_ranges(r.row_coverage(row) for r in readings if r.row_coverage(row))
-    if len(rc) == 1:
-        low, high = rc[0]
-        if low > 0 or high < 4_000_000:
-            res2 = (0 if low > 0 else high + 1) * 4_000_000 + row
-            break
-    else:
-        res2 = (rc[0][1] + 1) * 4_000_000 + row
-        break
-print(f"Result 2: {res2}")
+res2 = next(
+    x * 4_000_000 + y
+    for r in reversed(readings)
+    for x, y in r.rim()
+    if not any(r_.covers(x, y) for r_ in readings)
+    and 0 <= x <= 4_000_000
+    and 0 <= y <= 4_000_000
+)
+print(f"Result 2: {res2} in {time.perf_counter() - t:.3f} s")
