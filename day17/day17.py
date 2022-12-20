f98700d31123ca73dd020f22695838571cbf4670
maflAT <13979550+maflAT@users.noreply.github.com>
2022-12-20 18:59:02 +0100

    empty files

---
+++ day17/day17.py
@@ -0,0 +1,10 @@
+import time
+
+t = time.perf_counter()
+input = open("input.txt").read()
+
+# -------------------------------------- Part 1 -------------------------------------- #
+# print(f"Result 1: {res1} in {time.perf_counter() - t:.3f} s")
+
+# -------------------------------------- Part 2: ------------------------------------- #
+# print(f"Result 2: {res2} in {time.perf_counter() - t:.3f} s")
---
+++ day19/day19.py
@@ -0,0 +1,11 @@
+from time import perf_counter
+
+t = perf_counter()
+# -------------------------------------- Part 1: ------------------------------------- #
+
+# print(f"Result 1: {res1} in {(perf_counter() - t) * 1000:.1f} ms")
+
+t = perf_counter()
+# -------------------------------------- Part 2: ------------------------------------- #
+
+# print(f"Result 2: {res2} in {(perf_counter() - t) * 1000:.1f} ms")
