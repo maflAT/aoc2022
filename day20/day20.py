1220e3108a89ef12a6a05550130432b55c872bd9
maflAT <13979550+maflAT@users.noreply.github.com>
2022-12-20 22:36:39 +0100

    day 20 optimizations

--- day20/day20.py
+++ day20/day20.py
@@ -1,25 +1,24 @@
-from itertools import product
+from itertools import chain, repeat
+from time import perf_counter
 
 # -------------------------------------- Part 1: ------------------------------------- #
+t = perf_counter()
 lut = {tag: num for tag, num in enumerate(int(l) for l in open("input.txt"))}
-length = len(lut) - 1
-tags = list(lut.keys())
-for tag, num in lut.items():
-    idx = tags.index(tag)
-    del tags[idx]
-    tags.insert((idx + num) % length, tag)
+tags = list(lut)
+for tag, n in lut.items():
+    tags.pop(idx := tags.index(tag))
+    tags.insert((idx + n) % len(tags), tag)
 nums = [lut[t] for t in tags]
 res1 = sum([nums[(nums.index(0) + o) % len(nums)] for o in [1000, 2000, 3000]])
-print(f"Result 1: {res1}")
+print(f"Result 1: {res1} in {(perf_counter() - t) * 1000:.0f} ms")
 
 # -------------------------------------- Part 2: ------------------------------------- #
-key = 811_589_153
-lut = {tag: num * key for tag, num in enumerate(int(l) for l in open("input.txt"))}
-tags = list(lut.keys())
-for _, (tag, num) in product(range(10), lut.items()):
-    idx = tags.index(tag)
-    del tags[idx]
-    tags.insert((idx + num) % length, tag)
+t = perf_counter()
+lut = {tag: n * 811_589_153 for tag, n in enumerate(int(l) for l in open("input.txt"))}
+tags = list(lut)
+for tag, n in chain.from_iterable(repeat(lut.items(), 10)):
+    tags.pop(idx := tags.index(tag))
+    tags.insert((idx + n) % len(tags), tag)
 nums = [lut[t] for t in tags]
-res2 = sum([nums[(nums.index(0) + o) % len(nums)] for o in [1000, 2000, 3000]])
-print(f"Result 2: {res2}")
+res2 = sum(nums[(nums.index(0) + o) % len(nums)] for o in [1000, 2000, 3000])
+print(f"Result 2: {res2} in {(perf_counter() - t) * 1000:.0f} ms")
