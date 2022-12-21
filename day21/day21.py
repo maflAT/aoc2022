091544e98169120565e6483c4b1f2fe7f67fb60d
Markus Fleischhacker <markus.fleischhacker@magna.com>
2022-12-21 10:44:40 +0100

    day 21 part 2?

--- day21/day21.py
+++ day21/day21.py
@@ -1,6 +1,6 @@
 from time import perf_counter
 
-operations = {
+op_lut = {
     "+": lambda a, b: a + b,
     "-": lambda a, b: a - b,
     "*": lambda a, b: a * b,
@@ -9,7 +9,7 @@ operations = {
 t = perf_counter()
 # -------------------------------------- Part 1: ------------------------------------- #
 values: dict[str, int] = {}
-operands: dict[str, tuple[str, str]] = {}
+children: dict[str, tuple[str, str]] = {}
 ops: dict[str, str] = {}
 monkeys: set[str] = set()
 for l in open("input.txt"):
@@ -19,20 +19,61 @@ for l in open("input.txt"):
     else:
         monkeys.add(name)
         a, op, b = rest.split()
-        operands[name] = (a, b)
+        children[name] = (a, b)
         ops[name] = op
-while monkeys:
+values.pop("humn")
+new = None
+while (prev := new) != (new := len(monkeys)):
     for m in monkeys.copy():
         if m in values:
             continue
-        a, b = operands[m]
+        a, b = children[m]
         if a in values and b in values:
-            values[m] = operations[ops[m]](values[a], values[b])
+            values[m] = op_lut[ops[m]](values[a], values[b])
             monkeys.remove(m)
-res1 = values["root"]
-print(f"Result 1: {res1} in {(perf_counter() - t) * 1000:.1f} ms")
-
-# t = perf_counter()
+# [print(f"{k}: {v}") for k, v in values.items()]
 # -------------------------------------- Part 2: ------------------------------------- #
+monkeys.add("humn")
 
-# print(f"Result 2: {res2} in {(perf_counter() - t) * 1000:.1f} ms")
+
+def solve_children(parent: str) -> int:
+    vp = values[parent]
+    if parent == "humn":
+        return vp
+    ca, cb = children[parent]
+    if va := values.get(ca):
+        match ops[parent]:
+            case "+":
+                vb = vp - va
+            case "-":
+                vb = va - vp
+            case "*":
+                vb = vp // va
+            case "/":
+                vb = va // vp
+        values[cb] = vb
+        return solve_children(cb)
+    else:
+        vb = values[cb]
+        match ops[parent]:
+            case "+":
+                va = vp - vb
+            case "-":
+                va = vp + vb
+            case "*":
+                va = vp // vb
+            case "/":
+                va = vp * vb
+        values[ca] = va
+        return solve_children(ca)
+
+
+c1, c2 = children["root"]
+if values.get(c1):
+    values[c2] = values[c1]
+    res2 = solve_children(c2)
+else:
+    values[c1] = values[c2]
+    res2 = solve_children(c1)
+
+print(f"Result 2: {res2} in {(perf_counter() - t) * 1000:.1f} ms")
